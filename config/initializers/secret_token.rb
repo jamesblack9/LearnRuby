c0e364a134a0a4042c4f69b30a35f32dfb0ee046
jamesblack9 <jamesblack9@gmail.com>
2014-02-06 13:40:10 +0100

    Init Ruby Rail Prj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todos::Application.config.secret_key_base = 'b8640d01f3c5e984c399bdb02f13106b24f9142e714af08faced5ba1dcd4ec58a47e952263b8387a99d3052be984e3eec014ef6414a73ac668f01b19d61ccd59'
